4b437266b9b0801314e1f504e4b0a6648085742f
Tyndarius1 <kb.dacera@mlgcl.edu.ph>
2026-03-21 21:40:50 +0800

    Phase 06 Completed

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,7 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-//API Routes
 use App\Http\Controllers\Api\AuthController;
-
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
+use App\Http\Controllers\Api\CategoryController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -21,33 +10,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/staff', [AuthController::class, 'createStaff'])->middleware('role:admin');
 
-    Route::get('/admin-only', function () {
-        return response()->json([
-            'status' => true,
-            'message' => 'Welcome Admin',
-        ]);
-    })->middleware('role:admin');
-
-    Route::get('/staff-only', function () {
-        return response()->json([
-            'status' => true,
-            'message' => 'Welcome Staff',
-        ]);
-    })->middleware('role:staff');
-
-    Route::get('/user-only', function () {
-        return response()->json([
-            'status' => true,
-            'message' => 'Welcome User',
-        ]);
-    })->middleware('role:user');
-
-    Route::get('/admin-or-staff', function () {
-        return response()->json([
-            'status' => true,
-            'message' => 'Welcome Admin or Staff',
-        ]);
-    })->middleware('role:admin,staff');
+    Route::middleware('role:admin')->group(function () {
+        Route::apiResource('categories', CategoryController::class);
+    });
 });
