72c70b6fb66fa59e9b47624c418e722f28865646
Tyndarius1 <kb.dacera@mlgcl.edu.ph>
2026-03-21 21:02:07 +0800

    Phase 03 Refactored

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,17 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => 'password123',
+            'role' => 'admin',
+        ]);
+
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => 'password123',
+            'role' => 'user',
         ]);
     }
 }
